de7360a2125815b9c8eb9502f15b7158871d4de0
Harish Devarayal <harish.devarayal@nichi.com>
2024-09-23 16:56:31 +0530

    changes

--- src/app/company/list/page.js
+++ src/app/company/list/page.js
@@ -664,6 +664,7 @@ export default function CompanyList() {
             }
           }
           setLoading(false);
+          setImportModal(false);
           setCsvUploadInitiated(() => null);
         }
       }
