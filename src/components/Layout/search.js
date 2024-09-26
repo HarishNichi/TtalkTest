fd46a7547f3cf0f41f54ed38639a16fcdacf9937
Harish Devarayal <harish.devarayal@nichi.com>
2024-09-26 11:42:53 +0530

    changes

--- src/components/Layout/search.js
+++ src/components/Layout/search.js
@@ -12,7 +12,7 @@ export default function SearchInput({
       <input
         id="searchBarDashboard"
         type="text"
-        className={`w-full md:min-w-[100px] lg:min-w-[100px] border border-[#E7E7E9] flex flex-auto md:flex-1  p-2 text-xs   bg-[white] h-[40px] rounded focus:outline-none placeholder-[#AEA8A8] 
+        className={`w-full md:min-w-[100px] lg:min-w-[100px] border border-[#E7E7E9] flex flex-auto md:flex-1  p-2   bg-[white] h-[40px] rounded focus:outline-none placeholder-[#AEA8A8] 
         placeholder:text-center md:placeholder:text-left md:placeholder:pl-0
         dark:text-black text-[16px]`}
         placeholder={placeholder}
